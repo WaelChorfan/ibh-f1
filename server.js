1113c7837a9616034c947fc54544847181873147
Wael Chorfan <36275385+WaelChorfan@users.noreply.github.com>
2019-07-29 20:51:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,10 @@ var ejs = require('ejs');
 const path = require('path');
 var app = express();
 
-
 //configuration seveur express
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('views', __dirname + '/views');  
 app.set('view engine','ejs');
-
 
 //configuration db
 var connection = mysql.createConnection({
@@ -29,8 +27,18 @@ connection.connect();
   function (req,res) {
     connection.query('SELECT * FROM personnes ', function (error, result) {
       console.log(__dirname);
-      console.log(result.length);
       res.render('index',{msg:'hello world' ,personnes:result,long:result.length});
+
+    });
+     
+      
+  }
+  )
+  app.get('/delete:id',
+  function (req,res) {
+    connection.query('delete FROM personnes where id= '+req.params.id, function (error, result) {
+      
+      res.render('ajoute',{msg:req.params.id+'supprimé avec succés'});
 
     });
      
@@ -42,7 +50,6 @@ connection.connect();
 
      console.log(req.body);
 
-
     connection.query("INSERT INTO personnes(nom, prenom, tel) VALUES ('"+req.body.nom+"','"+
     req.body.prenom+"','"+
     req.body.tel+"')"
@@ -51,20 +58,29 @@ connection.connect();
       if (err) throw err;
       res.render('ajoute',{msg:'ajouté avec succes'})
     })
+  })
+  
 
+  
+app.post('/deletePersonne',
+function (req,res) {
+ 
+  var q= 'delete FROM personnes where id=' +req.body.id;
+  console.log(q)
+  connection.query( q, function (error, result) {
+   
+    res.send(req.body.id+'deleted successfully')
+
+  });
     
-  }
-  
-  
-  )
-
-
+}
+)
 
 
  
 app.listen(3000);
 
-
  
 console.log('The server is up and running at http://localhost:3000');
+
 
